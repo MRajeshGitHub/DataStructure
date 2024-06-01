3d85567307c5ffea88d8a8ec9549c516773bbc8a
ADMIN <ADMIN@DESKTOP-BFTQ2K6>
2024-06-01 17:19:07 +0530

    Welcome page Data structures...

---
+++ src/com/virtusa/DataStructrueTest.java
@@ -0,0 +1,11 @@
+package com.virtusa;
+
+public class DataStructrueTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Welcome to data Structures....");
+	}
+
+}
